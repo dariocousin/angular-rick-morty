c4a70f7aed4d90e7bb4146fe5fb04a0a963e4961
dariocousin <darioblascoalcolado@gmail.com>
2020-11-16 17:08:51 +0100

    Primeros cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MenuComponent } from './core/components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
